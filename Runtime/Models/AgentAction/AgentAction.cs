8533bf9d78833b4cbf5fcc1fb323a4a24865fe18
Henrik Thue Oelgaard <DeveloperOelgaard@gmail.com>
2022-04-07 13:34:31 +0200

    Hot fix

--- Runtime/Models/AgentAction/AgentAction.cs
+++ Runtime/Models/AgentAction/AgentAction.cs
@@ -34,7 +34,7 @@ public abstract class AgentAction: AiObjectModel
     }
     protected override AiObjectModel InternalClone()
     {
-        var clone = (AgentAction)Activator.CreateInstance(GetType(), this);
+        var clone = (AgentAction)Activator.CreateInstance(GetType());
         clone.Parameters = new List<Parameter>();
         foreach (var s in this.Parameters)
         {

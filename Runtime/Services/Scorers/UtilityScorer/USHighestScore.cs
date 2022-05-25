ab4e7223d98c846ec3886dcfc3d14bd8bfd675e7
Henrik Thue Oelgaard <DeveloperOelgaard@gmail.com>
2022-05-25 20:12:04 +0200

    Added: UCHighest and UCLowest score

---
+++ Runtime/Services/Scorers/UtilityScorer/USHighestScore.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace Services.Scorers.UtilityScorer
+{
+    public class USHighestScore: IUtilityScorer
+    {
+        private readonly string name = "Highest Score";
+        private readonly string description = "Returns the highest score among the considerations";
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public string GetDescription()
+        {
+            return description;
+        }
+
+        public float CalculateUtility(List<Consideration> considerations, IAiContext context)
+        {
+            var highestScore = float.MinValue;
+            foreach (var consideration in considerations)
+            {
+                var score = consideration.CalculateScore(context);
+                if (score <= 0)
+                {
+                    return 0;
+                }
+                if(!consideration.IsScorer) continue;
+                if (score > highestScore)
+                {
+                    highestScore = score;
+                }
+            }
+
+            return highestScore;
+        }
+    }
+}
---
+++ Runtime/Services/Scorers/UtilityScorer/USLowestScore.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace Services.Scorers.UtilityScorer
+{
+    public class USLowestScore: IUtilityScorer
+    {
+        private readonly string name = "Lowest Score";
+        private readonly string description = "Returns the lowest score among the considerations";
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public string GetDescription()
+        {
+            return description;
+        }
+
+        public float CalculateUtility(List<Consideration> considerations, IAiContext context)
+        {
+            var lowest = float.MaxValue;
+            foreach (var consideration in considerations)
+            {
+                var score = consideration.CalculateScore(context);
+                if (score <= 0)
+                {
+                    return 0;
+                }
+                if(!consideration.IsScorer) continue;
+                if (score < lowest)
+                {
+                    lowest = score;
+                }
+            }
+
+            return lowest;
+        }
+    }
+}

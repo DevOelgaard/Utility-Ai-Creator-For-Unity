d41b7721424ebd42e4ddebf212db907704aa2358
Henrik Thue Oelgaard <DeveloperOelgaard@gmail.com>
2022-10-22 14:50:09 +0200

    Added Demo Files

---
+++ Runtime/Demo/AgentActions/Demo_PrintTxt.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Demo_PrintTxt: AgentAction
+{
+    public Demo_PrintTxt()
+    {
+        // Add parameters in the constructor
+        AddParameter("Text to print","");
+    }
+
+    public override void OnStart(IAiContext context)
+    {
+        // Read the value of the parameter defined above. The parameterName is case sensitive
+        var textFromParameter = ParameterContainer.GetParamString("Text to print").Value;
+        
+        // Get the agent from the Context and cast him as AgentMono
+        // Don't think to much about the casting to AgentMono, just do it ;)
+        var agent = context.Agent as AgentMono;
+        
+        // Printing to the console
+        Debug.Log(agent.name +": " + textFromParameter);
+    }
+}
---
+++ Runtime/Models/AgentAction/Demo_AllParametersAction.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class Demo_AllParametersAction: AgentAction
+{
+    public Demo_AllParametersAction()
+    {
+        HelpText = "A spawn point must be set in the AiContext " +
+           "A Unit type must spawning must either be selected in parameters or in code";
+        AddParameter("Bool", true);
+        AddParameter("Enum",PerformanceTag.High);
+        AddParameter("Float",1f);
+        AddParameter("Int",100);
+        AddParameter("String","Hi you");
+        AddParameter("Color",Color.blue);
+    }
+
+    public override void OnStart(IAiContext context)
+    {
+        var b = ParameterContainer.GetParamBool("Bool");
+        var c = ParameterContainer.GetParamColor("Color");
+        var e = ParameterContainer.GetParamEnum("Enum");
+        var f = ParameterContainer.GetParamFloat("Float");
+        var i = ParameterContainer.GetParamInt("Int");
+        var s = ParameterContainer.GetParamString("String");
+    }
+}
+
